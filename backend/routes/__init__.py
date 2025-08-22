79b70e085ae00e4303d099179d9eada4c76afa9b
Wonka-BEEP <majalake@gmail.com>
2025-08-22 17:51:30 -0400

    Udapte __init__.py

--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -2,5 +2,7 @@
 from backend.routes.auth import auth_bp
 from backend.routes.users import users_bp
 from backend.routes.documents import docs_bp
+from backend.routes.audits import audits_bp
+from backend.routes.surveys import surveys_bp
 
-all_blueprints = [auth_bp, users_bp, docs_bp]
+all_blueprints = [auth_bp, users_bp, docs_bp, audits_bp, surveys_bp]
