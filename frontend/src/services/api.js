d3a5498e3e67764e7093e8d83db163e8562447f4
Gaalc <wheatdesu@gmail.com>
2025-11-11 16:22:40 -0300

    Upgrade api.js
    integracion con cloudflare

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,12 @@
 import axios from "axios";
 
 //api.js
-const api = axios.create({
-  baseURL: "http://127.0.0.1:5000/api",
-});
+const baseURL =
+  window.location.hostname === "localhost"
+    ? "http://127.0.0.1:5000/api"
+    : "https://scuba-brussels-efficient-combined.trycloudflare.com/api"; // tu URL del túnel de backend
+
+const api = axios.create({ baseURL });
 
 /* -------- request: añade token -------- */
 api.interceptors.request.use((config) => {
